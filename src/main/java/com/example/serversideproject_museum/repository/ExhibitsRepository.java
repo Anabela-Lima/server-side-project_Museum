5fd4120a9932de3bd6d1ea6d36e1c6e65fb2d490
Ana Bela Lima <a.blima@outlook.com>
2022-05-23 11:30:52 +0100

    @Repository annotation added

--- src/main/java/com/example/serversideproject_museum/repository/ExhibitsRepository.java
+++ src/main/java/com/example/serversideproject_museum/repository/ExhibitsRepository.java
@@ -2,7 +2,9 @@ package com.example.serversideproject_museum.repository;
 
 import com.example.serversideproject_museum.model.Exhibit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExhibitsRepository extends JpaRepository<Exhibit, Long> {
 
 
