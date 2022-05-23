fb40536e6007c321f47463e34baa9e1923d5a268
Falak Khan <falakalainkhan@gmail.com>
2022-05-23 11:09:11 +0100

    updated exhibit controller

--- src/main/java/com/example/serversideproject_museum/controller/ExhibitController.java
+++ src/main/java/com/example/serversideproject_museum/controller/ExhibitController.java
@@ -1,2 +1,51 @@
-package com.example.serversideproject_museum.controller;public class ExhibitController {
+package com.example.serversideproject_museum.controller;
+
+import com.example.serversideproject_museum.model.Exhibit;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ExhibitController {
+
+    public final ExhibitRepository exhibitRepository;
+
+    public ExhibitController(ExhibitRepository exhibitRepository) {
+        this.exhibitRepository = exhibitRepository;
+    }
+
+    @GetMapping("/exhibits")
+    public ResponseEntity<List<Exhibit>> getAllExhibits(){
+        List<Exhibit> exhibits = exhibitRepository.findAll();
+        return ResponseEntity.ok().body(exhibits);
+    }
+
+    @PostMapping("/exhibits")
+    public ResponseEntity<Exhibit> addExhibit(@RequestBody Exhibit exhibit){
+        Exhibit newExhibit = ExhibitRepository.save(exhibit);
+        return ResponseEntity.ok().body(newExhibit);
+    }
+
+    @PutMapping("exhibits/{id}")
+    public ResponseEntity<Exhibit> updateExhibit(@RequestBody Exhibit exhibit, @PathVariable Long id){
+        Exhibit update = exhibitRepository.findbyId(id).map(updatedExhibit -> {
+                updateExhibit.setName(exhibit.getName());
+                return exhibitRepository.save(updatedExhibit);})
+        .orElseGet(() -> {return ExhibitRepository.save(exhibit);});
+        return ResponseEntity.ok().body(update);
+    }
+
+    @DeleteMapping("exhibits/{id}")
+    public ResponseEntity<String> deleteArtifact(@PathVariable Long id){
+        exhibitRepository.getById(id);
+        return ResponseEntity.ok("Exhibit with id" +id +" has been removed from database.");
+    }
+
+
+
+
+
 }
+
+
