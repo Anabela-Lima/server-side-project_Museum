53b2b64e5cefe8f70d773a0f4fc45987e89c5c14
James Stillman <jamesstillman99@gmail.com>
2022-05-23 09:16:59 +0100

    Spring project initialized

---
+++ src/test/java/com/example/serversideproject_museum/ServerSideProjectMuseumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serversideproject_museum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideProjectMuseumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
