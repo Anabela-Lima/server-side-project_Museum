337187856b2113789c42248d56fa75f8197badf9
James Stillman <jamesstillman99@gmail.com>
2022-05-23 11:27:01 +0100

    Created Get mapping for MuseumController

--- src/main/java/com/example/serversideproject_museum/controller/MuseumController.java
+++ src/main/java/com/example/serversideproject_museum/controller/MuseumController.java
@@ -1,11 +1,25 @@
 package com.example.serversideproject_museum.controller;
 
+import com.example.serversideproject_museum.model.Museum;
+import com.example.serversideproject_museum.service.MuseumService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/museum")
 public class MuseumController {
 
+    private final MuseumService museumService;
 
+    public MuseumController(MuseumService museumService){
+        this.museumService = museumService;
+    }
+    @GetMapping
+    public List<Museum> getMuseums(){
+        return museumService.findAll();
+    }
 }
