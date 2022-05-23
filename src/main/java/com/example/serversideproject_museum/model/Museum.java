dac768a41e341448df98aa41cd0811a3b4c1540b
James Stillman <jamesstillman99@gmail.com>
2022-05-23 10:07:11 +0100

    Museum fields added

--- src/main/java/com/example/serversideproject_museum/model/Museum.java
+++ src/main/java/com/example/serversideproject_museum/model/Museum.java
@@ -1,6 +1,11 @@
 package com.example.serversideproject_museum.model;
 
+import java.util.Set;
+
 public class Museum {
 
-
+    private Long id;
+    private String name;
+    private Set<Exhibit> exhibits;
+    //private Set<Staff> staff;
 }
