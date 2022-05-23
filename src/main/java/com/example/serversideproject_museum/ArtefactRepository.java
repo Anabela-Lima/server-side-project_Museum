c62a96fb7f37de9eb4876ce0c345ea39d8e729f7
Maisha Islam <maisha.b.islam@gmail.com>
2022-05-23 11:43:08 +0100

    Artefact repository amended

--- src/main/java/com/example/serversideproject_museum/ArtefactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.serversideproject_museum;
-
-import com.example.serversideproject_museum.model.Artefact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtefactRepository extends JpaRepository<Artefact, Long> {
-}
