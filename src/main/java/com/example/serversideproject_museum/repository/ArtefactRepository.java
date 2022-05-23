32bd92b94cf552400ebf467372dff1b28e23f19c
Ana Bela Lima <a.blima@outlook.com>
2022-05-23 11:34:38 +0100

    @Repository annotation added

--- src/main/java/com/example/serversideproject_museum/repository/ArtefactRepository.java
+++ src/main/java/com/example/serversideproject_museum/repository/ArtefactRepository.java
@@ -2,6 +2,8 @@ package com.example.serversideproject_museum.repository;
 
 import com.example.serversideproject_museum.model.Artefact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArtefactRepository extends JpaRepository<Artefact, Long> {
 }
--- src/main/java/com/example/serversideproject_museum/repository/MuseumRepository.java
+++ src/main/java/com/example/serversideproject_museum/repository/MuseumRepository.java
@@ -2,7 +2,9 @@ package com.example.serversideproject_museum.repository;
 
 import com.example.serversideproject_museum.model.Museum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MuseumRepository extends JpaRepository<Museum, Long> {
 
 
