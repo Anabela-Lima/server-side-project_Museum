f0626193bf2d97e77d89b5c3cbd72750e44d4163
James Stillman <jamesstillman99@gmail.com>
2022-05-24 15:01:14 +0100

    Bug fix on museum properties

--- src/main/java/com/example/serversideproject_museum/model/Museum.java
+++ src/main/java/com/example/serversideproject_museum/model/Museum.java
@@ -1,6 +1,7 @@
 package com.example.serversideproject_museum.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -21,7 +22,7 @@ public class Museum {
 
     @Enumerated(EnumType.STRING)
     private Country country;
-    @JsonIgnore
+    @JsonIgnoreProperties(value = "museum")
     @OneToMany(mappedBy = "museum", cascade = ALL)
     private Set<Exhibit> exhibits = new HashSet<>();
     //private Set<Staff> staff;
