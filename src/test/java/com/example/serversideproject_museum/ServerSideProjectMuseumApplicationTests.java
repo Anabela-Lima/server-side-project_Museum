b0fb321cb24d27edb229c979ff50fd801dce97bc
Falak Khan <falakalainkhan@gmail.com>
2022-05-26 21:42:15 +0100

    fixed tests

--- src/test/java/com/example/serversideproject_museum/ServerSideProjectMuseumApplicationTests.java
+++ src/test/java/com/example/serversideproject_museum/ServerSideProjectMuseumApplicationTests.java
@@ -7,21 +7,19 @@ import com.example.serversideproject_museum.repository.MuseumRepository;
 import com.example.serversideproject_museum.repository.StaffRepository;
 import com.example.serversideproject_museum.service.ExhibitService;
 import com.example.serversideproject_museum.service.MuseumService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 import javax.transaction.Transactional;
 import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -51,7 +49,7 @@ class ServerSideProjectMuseumApplicationTests {
 
     @Nested
     @DisplayName("Tests for Museum")
-    class MuseumTests{
+    class MuseumTests {
 
         @Transactional
         @Test
@@ -73,7 +71,7 @@ class ServerSideProjectMuseumApplicationTests {
         @Transactional
         @Test
         @DisplayName("Can add a new museum")
-        public void canAddMuseum(){
+        public void canAddMuseum() {
             museumService.addMuseum("New Museum test", Country.Afghanistan);
             assertEquals(21, museumRepository.findAll().size());
         }
@@ -81,14 +79,14 @@ class ServerSideProjectMuseumApplicationTests {
         @Transactional
         @Test
         @DisplayName("Can filter museums by country")
-        public void canFindByCountry(){
+        public void canFindByCountry() {
             assertEquals(1, museumService.findByCountry(Country.Japan).size());
         }
 
         @Transactional
         @Test
         @DisplayName("Can add existing exhibit to the museum")
-        public void canAddExhibitToMuseum(){
+        public void canAddExhibitToMuseum() {
             Museum museum = museumService.addExhibit(1L, 20L);
             assertEquals(5, museum.getExhibits().size());
         }
@@ -96,7 +94,7 @@ class ServerSideProjectMuseumApplicationTests {
         @Transactional
         @Test
         @DisplayName("Can add a new exhibit to the museum")
-        public void canAddNewExhibitToMuseum(){
+        public void canAddNewExhibitToMuseum() {
             Exhibit exhibit = exhibitRepository.save(new Exhibit("Test Exhibit"));
             Museum museum = museumService.addExhibit(1L, 28L);
             assertEquals(5, museum.getExhibits().size());
@@ -105,7 +103,7 @@ class ServerSideProjectMuseumApplicationTests {
         @Transactional
         @Test
         @DisplayName("Can add existing exhibit to new museum")
-        public void canAddExhibitToNewMuseum(){
+        public void canAddExhibitToNewMuseum() {
             Museum museum = museumService.addMuseum("Test Museum", Country.Afghanistan);
             assertEquals(0, museum.getExhibits().size());
             Museum updatedMuseum = museumService.addExhibit(21L, 1L);
@@ -115,7 +113,7 @@ class ServerSideProjectMuseumApplicationTests {
         @Transactional
         @Test
         @DisplayName("Can add new exhibit to new museum")
-        public void canAddNewExhibitToNewMuseum(){
+        public void canAddNewExhibitToNewMuseum() {
             Museum museum = museumService.addMuseum("Test Museum", Country.Afghanistan);
             Exhibit exhibit = exhibitRepository.save(new Exhibit("Test Exhibit"));
             assertEquals(21, museumRepository.findAll().size());
@@ -131,7 +129,7 @@ class ServerSideProjectMuseumApplicationTests {
 
     @Nested
     @DisplayName("Tests for Artefacts")
-    class ArtefactTest{
+    class ArtefactTest {
         @Transactional
         @Test
         public void dataSqlLoadsArtefacts() {
@@ -143,8 +141,8 @@ class ServerSideProjectMuseumApplicationTests {
         @Test
         @DisplayName("Can find artefacts by country")
         public void canFindByCountry() {
-            List<Artefact> found =  artefactRepository.findByCountry(Country.UnitedStates);
-            for (Artefact artefact: found) {
+            List<Artefact> found = artefactRepository.findByCountry(Country.UnitedStates);
+            for (Artefact artefact : found) {
                 assertEquals("UnitedStates", artefact.getCountry().name());
             }
         }
@@ -152,7 +150,7 @@ class ServerSideProjectMuseumApplicationTests {
 
     @Nested
     @DisplayName("Tests for Staff")
-    class StaffTest{
+    class StaffTest {
         @Transactional
         @Test
         @DisplayName("Can fire staff")
@@ -165,7 +163,7 @@ class ServerSideProjectMuseumApplicationTests {
 
     @Nested
     @DisplayName("Tests for Exhibit")
-    class ExhibitTest{
+    class ExhibitTest {
         @Transactional
         @Test
         @DisplayName("Exhibits load properly from data.sql")
@@ -173,6 +171,7 @@ class ServerSideProjectMuseumApplicationTests {
             List<Exhibit> found = exhibitRepository.findAll();
             assertEquals(27, found.size());
         }
+
         @Transactional
         @Test
         @DisplayName("Can find exhibits by ID")
@@ -181,7 +180,6 @@ class ServerSideProjectMuseumApplicationTests {
             assertEquals("Ancient Egypt", found.getName());
         }
     }
-
 
 
 }
